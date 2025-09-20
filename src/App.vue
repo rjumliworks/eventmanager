d7bdcdcc4230d1c54970a2f51df26b836dbd940d
imkrad <164296287+imkrad@users.noreply.github.com>
2025-09-20 13:25:39 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
-    <router-view></router-view>
+    <div id="app" :class="{ 'safe-area': isIos }">
+        <router-view></router-view>
+    </div>
 </template>
 
 <script>
     import { mapActions } from 'vuex'
+    import { Capacitor } from '@capacitor/core'
     export default {
         name: 'App',
+        data() {
+            return {
+                isIos: Capacitor.getPlatform() === 'ios'
+            }
+        },
         async created() {
             const token = localStorage.getItem('token');
             if (token) {
@@ -25,3 +33,16 @@
         }
     }
 </script>
+
+<style>
+.safe-area {
+  /* Add safe area on iOS automatically */
+  padding-top: calc(16px + env(safe-area-inset-top));
+  padding-left: env(safe-area-inset-left);
+  padding-right: env(safe-area-inset-right);
+  padding-bottom: env(safe-area-inset-bottom);
+
+  /* Fallback for older iOS */
+  padding-top: calc(16px + constant(safe-area-inset-top));
+}
+</style>
