b5b49df5559698805c23b3f4b1ba5f98b4fcfc14
imkrad <164296287+imkrad@users.noreply.github.com>
2025-09-20 13:28:15 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,47 @@
 <template>
-    <div id="app" :class="{ 'safe-area': isIos }">
-        <router-view></router-view>
-    </div>
+  <div id="app" :class="{ 'safe-area': applySafeArea }">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-    import { mapActions } from 'vuex'
-    import { Capacitor } from '@capacitor/core'
-    export default {
-        name: 'App',
-        data() {
-            return {
-                isIos: Capacitor.getPlatform() === 'ios'
-            }
-        },
-        async created() {
-            const token = localStorage.getItem('token');
-            if (token) {
-                try {
-                    await Promise.all([
-                        this.loadData(),
-                    ]);
-                } catch (err) {
-                    console.error('Failed to preload data', err);
-                }
-            }
-        },
-        methods: {
-            ...mapActions({
-                loadData: 'data/loadInitialData',
-            })
-        }
+import { mapActions } from 'vuex'
+import { Capacitor } from '@capacitor/core'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      isIos: Capacitor.getPlatform() === 'ios'
     }
+  },
+  computed: {
+    applySafeArea() {
+      return this.isIos && this.$route.path !== '/dashboard'
+    }
+  },
+  async created() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      try {
+        await Promise.all([
+          this.loadData(),
+        ]);
+      } catch (err) {
+        console.error('Failed to preload data', err);
+      }
+    }
+  },
+  methods: {
+    ...mapActions({
+      loadData: 'data/loadInitialData',
+    })
+  }
+}
 </script>
 
 <style>
 .safe-area {
-  /* Add safe area on iOS automatically */
   padding-top: calc(16px + env(safe-area-inset-top));
   padding-left: env(safe-area-inset-left);
   padding-right: env(safe-area-inset-right);
